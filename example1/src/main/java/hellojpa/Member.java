e4a98fabd8385cacb1b89fb698edfc239f82348e
woosung1223 <dntjd991223@naver.com>
2023-06-24 14:08:20 +0900

    example1: 값타입 컬렉션 실습

--- example1/src/main/java/hellojpa/Member.java
+++ example1/src/main/java/hellojpa/Member.java
@@ -3,7 +3,9 @@ package hellojpa;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -12,6 +14,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -29,7 +35,14 @@ public class Member {
     private Team team;
 
     @Embedded
-    private Period workPeriod;
-    @Embedded
     private Address homeAddress;
+
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    @Column(name = "FOOD_NAME")
+    private List<String> favoriteFoods = new ArrayList<>();
+
+    @ElementCollection
+    @CollectionTable(name = "ADDRESS", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    private List<Address> addressHistory = new ArrayList<>();
 }
